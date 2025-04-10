5df3cb3df6704adc60d7a80e2de13fa867f58b58
ThiSiniero <thiagosiniero@gmail.com>
2025-04-10 17:05:04 -0300

    att3 responsive navibar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,11 +40,11 @@ function Navbar() {
         <div className="lg:hidden w-full flex justify-between items-center px-4">
           <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
             {isOpen ? (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 sm:h-10 sm:w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
               </svg>
             ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 sm:h-12 sm:w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
             )}  
